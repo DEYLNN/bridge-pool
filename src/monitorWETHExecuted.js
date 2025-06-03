aa41d0a0c16c6c0fbf756e281ef09d9e5e6086eb
DEYLNN <danimansapekalongan@gmail.com>
2025-06-03 10:37:27 +0700

    update 1.7

--- src/monitorWETHExecuted.js
+++ src/monitorWETHExecuted.js
@@ -3,17 +3,20 @@ const transferWETH = require("./transferWETH");
 
 async function monitorWETHExecuted(provider, wallet, wethAddress, walletUtamaAddress, maxTry = 20) {
   let retry = 0;
+  let b = { weth: 0 }; // Initial value biar gak error kalau getBalances fail
   while (retry < maxTry) {
-    const b = await getBalances(provider, wallet.address, wethAddress);
+    b = await getBalances(provider, wallet.address, wethAddress);
     if (parseFloat(b.weth) === 0) {
       return true;
     }
     await new Promise(r => setTimeout(r, 10000));
     retry++;
   }
-  // Jika sampai di sini, artinya WETH belum 0, force kirim balik ke wallet utama!
+  // Sampai sini, b.weth udah pasti ada (hasil getBalances terakhir)
   await transferWETH(wallet, wethAddress, walletUtamaAddress, b.weth);
   return false;
 }
 
 module.exports = monitorWETHExecuted;
+
+//
